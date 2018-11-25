bdebe9ae5993dc2320979c3fb0c066f7f8bd0063
NathanDanTaylor <thenathanforall@gmail.com>
2018-11-25 15:35:49 -0500

    my first print

--- src/hello.java
+++ src/hello.java
@@ -1,4 +1,32 @@
 
 public class hello {
 
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("Stan LOONA");
+		System.out.print("Nayeon");
+		System.out.print("TWICE");
+		
+	String nataString = "nata";
+	String youtubeNata= "internets";
+	int number = 5;
+	
+	
+	
+	System.out.println("\n"+youtubeNata+nataString+"   "+number);
+	System.out.println(number);
+	System.out.println(number+number);
+	add(number);
+	subtract(number);}
+
+	public static void add(int numberPassed) {
+		System.out.println(numberPassed+numberPassed);	
+	}
+	
+	
+	public static void subtract(int numberPassed) {
+		System.out.println(numberPassed-numberPassed);	
+	}
 }
